0e2927d68745e8b9340510537ebe3164087b9f65
Daniel Allen <dan@aeternagroup.net>
2026-08-31 01:08:00 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
 # --- aws_notifications_notification_configuration ---
-output "notifications_notification_configurations_id" {
-  description = "Map of id values across all notifications_notification_configurations, keyed the same as var.notifications_notification_configurations"
-  value       = module.notifications_notification_configurations.notifications_notification_configurations_id
-}
-
 output "notifications_notification_configurations_aggregation_duration" {
   description = "Map of aggregation_duration values across all notifications_notification_configurations, keyed the same as var.notifications_notification_configurations"
   value       = module.notifications_notification_configurations.notifications_notification_configurations_aggregation_duration
@@ -35,11 +30,6 @@ output "notifications_notification_configurations_tags_all" {
 }
 
 # --- aws_notifications_channel_association ---
-output "notifications_channel_associations_id" {
-  description = "Map of id values across all notifications_channel_associations, keyed the same as var.notifications_channel_associations"
-  value       = module.notifications_channel_associations.notifications_channel_associations_id
-}
-
 output "notifications_channel_associations_arn" {
   description = "Map of arn values across all notifications_channel_associations, keyed the same as var.notifications_channel_associations"
   value       = module.notifications_channel_associations.notifications_channel_associations_arn
@@ -51,11 +41,6 @@ output "notifications_channel_associations_notification_configuration_arn" {
 }
 
 # --- aws_notifications_event_rule ---
-output "notifications_event_rules_id" {
-  description = "Map of id values across all notifications_event_rules, keyed the same as var.notifications_event_rules"
-  value       = module.notifications_event_rules.notifications_event_rules_id
-}
-
 output "notifications_event_rules_arn" {
   description = "Map of arn values across all notifications_event_rules, keyed the same as var.notifications_event_rules"
   value       = module.notifications_event_rules.notifications_event_rules_arn
@@ -87,11 +72,6 @@ output "notifications_event_rules_source" {
 }
 
 # --- aws_notifications_organizational_unit_association ---
-output "notifications_organizational_unit_associations_id" {
-  description = "Map of id values across all notifications_organizational_unit_associations, keyed the same as var.notifications_organizational_unit_associations"
-  value       = module.notifications_organizational_unit_associations.notifications_organizational_unit_associations_id
-}
-
 output "notifications_organizational_unit_associations_notification_configuration_arn" {
   description = "Map of notification_configuration_arn values across all notifications_organizational_unit_associations, keyed the same as var.notifications_organizational_unit_associations"
   value       = module.notifications_organizational_unit_associations.notifications_organizational_unit_associations_notification_configuration_arn
